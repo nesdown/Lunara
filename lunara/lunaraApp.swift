a6d2a256b35cee3390a233c5b79b6f9e3fc9a54f
nesdown <glushko.bohdan@gmail.com>
2025-02-27 20:10:10 -0600

    Initial Commit

---
+++ lunara/lunaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  lunaraApp.swift
+//  lunara
+//
+//  Created by Bohdan Hlushko on 27.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct lunaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ lunaraTests/lunaraTests.swift
@@ -0,0 +1,17 @@
+//
+//  lunaraTests.swift
+//  lunaraTests
+//
+//  Created by Bohdan Hlushko on 27.02.2025.
+//
+
+import Testing
+@testable import lunara
+
+struct lunaraTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
